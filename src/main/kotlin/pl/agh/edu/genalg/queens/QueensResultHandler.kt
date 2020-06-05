81a1992f4534d630f3314f06c35f8b4ab60570b2
Kasper <kasper.kadzielawa@gmail.com>
2020-06-05 12:44:45 +0200

    Remove redundant line

--- src/main/kotlin/pl/agh/edu/genalg/queens/QueensResultHandler.kt
+++ src/main/kotlin/pl/agh/edu/genalg/queens/QueensResultHandler.kt
@@ -3,7 +3,6 @@ package pl.agh.edu.genalg.queens
 import pl.agh.edu.genalg.framework.flow.ResultHandler
 import pl.agh.edu.genalg.framework.metrics.Reporter
 import pl.agh.edu.genalg.framework.model.EvaluatedPopulation
-import pl.agh.edu.genalg.onemax.OneMaxHyperparameters
 
 class QueensResultHandler(hyperparameters: QueensHyperparameters, reporter: Reporter) :
     ResultHandler<Queens, EvaluatedQueens, QueensHyperparameters>(hyperparameters, reporter) {
