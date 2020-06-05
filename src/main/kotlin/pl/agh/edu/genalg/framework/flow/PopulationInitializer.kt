9768165cf73e478e4e80c7b707ea53a842bb8a11
Kasper <kasper.kadzielawa@gmail.com>
2020-06-05 12:44:09 +0200

    Refactor population initialization

--- src/main/kotlin/pl/agh/edu/genalg/framework/flow/PopulationInitializer.kt
+++ src/main/kotlin/pl/agh/edu/genalg/framework/flow/PopulationInitializer.kt
@@ -4,7 +4,15 @@ import pl.agh.edu.genalg.framework.metrics.Reporter
 import pl.agh.edu.genalg.framework.model.Entity
 import pl.agh.edu.genalg.framework.model.Hyperparameters
 import pl.agh.edu.genalg.framework.model.Population
+import pl.agh.edu.genalg.onemax.BinaryVector
 
 abstract class PopulationInitializer<E : Entity, H : Hyperparameters>(val hyperparameters: H, val reporter: Reporter) {
-    abstract fun initializePopulation(): Population<E>
+    fun initializePopulation(): Population<E> {
+        val entities = (1..hyperparameters.initialPopulationSize)
+            .map { initializeEntity() }
+
+        return Population(entities)
+    }
+
+    protected abstract fun initializeEntity(): E
 }
--- src/main/kotlin/pl/agh/edu/genalg/onemax/OneMaxPopulationInitializer.kt
+++ src/main/kotlin/pl/agh/edu/genalg/onemax/OneMaxPopulationInitializer.kt
@@ -2,27 +2,20 @@ package pl.agh.edu.genalg.onemax
 
 import pl.agh.edu.genalg.framework.metrics.Reporter
 import pl.agh.edu.genalg.framework.flow.PopulationInitializer
-import pl.agh.edu.genalg.framework.model.Population
+import kotlin.random.Random
 
 class OneMaxPopulationInitializer(hyperparameters: OneMaxHyperparameters, reporter: Reporter) :
     PopulationInitializer<BinaryVector, OneMaxHyperparameters>(hyperparameters, reporter) {
 
-    private val random = kotlin.random.Random.Default
+    override fun initializeEntity(): BinaryVector {
+        val randomVector = (1..hyperparameters.vectorSize)
+            .map { getRandomGene() }
+            .toIntArray()
 
-    override fun initializePopulation(): Population<BinaryVector> {
-        val entities = (1..hyperparameters.initialPopulationSize)
-            .map {
-                val randomVector = (1..hyperparameters.vectorSize)
-                    .map { getRandomGene() }
-                    .toIntArray()
-
-                BinaryVector(randomVector)
-            }
-
-        return Population(entities)
+        return BinaryVector(randomVector)
     }
 
     private fun getRandomGene(): Int {
-        return if (random.nextDouble() < 0.5) 1 else 0
+        return if (Random.nextDouble() < 0.5) 1 else 0
     }
 }
