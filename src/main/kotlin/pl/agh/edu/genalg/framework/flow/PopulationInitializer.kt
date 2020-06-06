76ce4dfc826edaaedb021bab3d01d95b79b5181a
Kasper <kasper.kadzielawa@gmail.com>
2020-06-06 15:34:07 +0200

    Remove redundant import

--- src/main/kotlin/pl/agh/edu/genalg/framework/flow/PopulationInitializer.kt
+++ src/main/kotlin/pl/agh/edu/genalg/framework/flow/PopulationInitializer.kt
@@ -4,7 +4,6 @@ import pl.agh.edu.genalg.framework.metrics.Reporter
 import pl.agh.edu.genalg.framework.model.Entity
 import pl.agh.edu.genalg.framework.model.Hyperparameters
 import pl.agh.edu.genalg.framework.model.Population
-import pl.agh.edu.genalg.onemax.BinaryVector
 
 abstract class PopulationInitializer<E : Entity, H : Hyperparameters>(val hyperparameters: H, val reporter: Reporter) {
     fun initializePopulation(): Population<E> {
